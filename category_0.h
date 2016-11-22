5fdf8deee4f5ad3a2a86cc159b552e12d6b2bc20
Bakushin10 <shnnagai@gmail.com>
2016-11-22 02:05:58 -0500

    Update category_0.h

--- category_0.h
+++ category_0.h
@@ -1,14 +1,17 @@
 #ifndef CATEGORY_0_H
 #define CATEGORY_0_H
 #include "simulation.h"
+#include <iostream>
+using namespace std;
+
 
 class category_0{
     public:
         category_0();
          ~category_0();
          int convertToDecimal_16bits(char* inst);
-         void show_category_0(int num, int address);
-         void getCode(char* inst,int inst_size,int address,simulation*,int ,bool);
+         void show_category_0(int num, int address,ofstream&);
+         void getCode(char*,int,int,simulation*,int,bool,ofstream&);
 
 
     protected:
@@ -19,4 +22,3 @@ class category_0{
 };
 
 #endif // CATEGORY_0_H
-
