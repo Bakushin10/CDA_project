8c5deee9b44a38a134080bd20086a7532f4022dc
Bakushin10 <shnnagai@gmail.com>
2016-11-22 02:10:02 -0500

    Update simulation.cpp

--- simulation.cpp
+++ simulation.cpp
@@ -3,32 +3,55 @@
 #include <stdlib.h>     /* atoi */
 #include <math.h>       /* pow */
 using namespace std;
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ios;
+
+#include<fstream>
+using std::ifstream;
+using std::ofstream;
 
 simulation::simulation(){
 
 }
 
 simulation::~simulation(){
-   delete registers;
+   delete data;
 }
 
-void simulation::setReg(int regSize){
-    this->registers = new int[regSize];
+void simulation::setReg(int dataSize){
+    this->data = new int[dataSize];
     this->reg_cnt = 0;
-    this->regSize = regSize;
+    this->dataSize = dataSize;
+    this->cycle = 1;///starting at 1
+}
+
+void simulation::setdata_starting_address(int num,int address){
+	this->data_starting_address = address+num*4;
 }
 
 void simulation::updateRegisters(int val){
-        registers[reg_cnt] = val;
+        data[reg_cnt] = val;
         reg_cnt++;
 }
 
-void simulation::showRegister(){
-
-    for(int i =0; i<this->regSize; i++) {
-        if(i%8 == 0)
-            std::cout<<"\n";
-        std::cout<<registers[i]<<"	";
-    }
+int simulation::getData(int dataAt){
+    return this->data[dataAt];
 }
 
+void simulation::showData(ofstream& myfile_sim){
+
+    myfile_sim<<"\n\nData";
+
+    int data_incre = data_starting_address;
+    for(int i =0; i<this->dataSize; i++) {
+        if(i%8 == 0)
+            myfile_sim<<"\n"<<data_incre<<":	";
+        myfile_sim<<data[i]<<"	";
+        data_incre = data_incre+4;
+    }
+    this->cycle++;
+    myfile_sim<<"\n";
+}
