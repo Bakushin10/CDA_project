ce08edd71df273c12a757697a1daef9943a526bb
Bakushin10 <shnnagai@gmail.com>
2016-11-22 02:10:20 -0500

    Update simulation.h

--- simulation.h
+++ simulation.h
@@ -1,24 +1,28 @@
 #ifndef SIMULATION_H
 #define SIMULATION_H
-
+#include <iostream>
+using namespace std;
 
 class simulation{
+    friend class ArrayList;
     public:
         simulation();
         ~simulation();
         void setReg(int);
         void updateRegisters(int);
-        void showRegister();
+        void showData(ofstream&);
+        int getData(int dataAt);
+	void setdata_starting_address(int num,int address);
 
     protected:
 
     private:
         int cycle;///keep track of cycle count
-        int* registers;///32 bits register
+        int* data;///32 bits register
         int reg_cnt;
-        int regSize;
+        int dataSize;
+        int data_starting_address;
 
 };
 
 #endif // SIMULATION_H
-
